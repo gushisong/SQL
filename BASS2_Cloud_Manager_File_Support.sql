c4124d19c7d3ab6fe89950522259640c96226402
gushisong@gmail.com <>
2017-12-01 00:22:24 -0700

    A

---
+++ BASS2_Cloud_Manager_File_Support.sql
@@ -0,0 +1,24 @@
+
+
+/*UOW_TYPECODEDEF New table , file reference typecode definitions of MO project*/
+
+
+select count(*) from PS_UOW_TYPECODEDEF;
+
+/*UOW_CC_NOSIMPL Compare Report of file reference in clearcase with conflict code change on parent
+branch, new field FILEREFTYPCODE is added.*/
+
+select * from PS_UOW_CC_NOSIMPL;
+
+
+/*UOW_CC_COMP_VW Compare report of file reference in clearcase with code change to it version 0, new
+field FILEREFTYPCODE is added.*/
+
+
+select * from PS_UOW_CC_COMP_VM;
+
+
+/*B2_SYNC_ENV Record for environment syncing, new field POPULATETYPECODE is added.*/
+
+
+select * from PS_B2_SYNC_ENV;
